efbd275bd5e799b82c06d82a2b7d3dc62bd59b1b
Petris Fernandes <petrisrf@gmail.com>
2026-01-09 01:21:41 -0300

    Isolated editor into his own page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,8 @@ import { useRef, useState, useEffect } from "react";
 import "./App.css";
 import { Editor } from "@monaco-editor/react";
 import "monaco-editor/esm/vs/editor/contrib/snippet/browser/snippetController2.js";
+import CodeEditor from "./components/CodeEditor";
 
-const defaultSpec = `RSpec.describe 'My First Spec' do 
-  it "is not correct" do 
-    expect(1+1).to eq(3)
-  end
-
-  it "is correct" do 
-    expect(1+1).to eq(2)
-  end
-end`;
 
 const OutputDisplay = ({ content }) => {
   let terminalColor = 'text-white';
@@ -31,83 +23,13 @@ const OutputDisplay = ({ content }) => {
 }
 
 const App = () => {
-  const workerRef = useRef(null);
-  const testEditor = useRef(null);
-  const [isRunning, setIsRunning] = useState(false);
-  const [terminalOutput, setTerminalOutput] = useState("");
-
-  useEffect(() => {
-    const worker = new Worker(
-      new URL("./ruby.worker.js", import.meta.url),
-      { type: "module" }
-    );
-
-    workerRef.current = worker;
-
-    worker.onmessage = (event) => {
-      const { type, result, message } = event.data;
-
-      if (type == 'READY') {
-        console.log('RUBY VM ready');
-      }
-
-      if (type === 'DONE') {
-        console.log(result);
-        setTerminalOutput(result)
-        setIsRunning(false);
-      }
-
-      if (type == 'ERROR') {
-        console.error(message)
-      }
-    }
-
-    return () => { worker.terminate(); }
-  }, [])
-
-  const runTestsClickHandler = (_) => {
-    if (isRunning) return;
-
-    setIsRunning(true);
-
-    workerRef.current.postMessage({
-      type: 'RUN',
-      spec: testEditor.current.getValue()
-    })
-  }
-
-  const onMountTestEditor = (editor) => {
-    testEditor.current = editor;
-  }
-
   return (
     <>
-      <div className="bg-zinc-800 size-full">
-        <Editor
-          ref={testEditor}
-          height="50vh"
-          width="49vw"
-          theme="vs-dark"
-          defaultLanguage="ruby"
-          defaultValue={defaultSpec}
-          options={{
-            minimap: { enabled: false },
-            tabSize: 2,
-            formatOnType: true,
-            insertSpaces: true,
-            autoIndent: "advanced",
-            scrollbar: { vertical: "hidden" },
-          }}
-          onMount={onMountTestEditor}
-        />
-        <OutputDisplay
-          content={terminalOutput} />
-        <button
-          className={`${isRunning ? 'bg-slate-300' : 'bg-green-500'} px-4 py-3 rounded text-white cursor-pointer`}
-          disabled={isRunning}
-          onClick={(_) => runTestsClickHandler()}
-        >Run Tests</button>
-        <button>Reset</button>
+      <div className="bg-zinc-800 size-full grid grid-cols-2">
+        <div>
+          Exercise Content
+        </div>
+        <div><CodeEditor /></div>
       </div>
     </>
   );
