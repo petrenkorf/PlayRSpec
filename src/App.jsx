7d62f99842abf624c5460dd38a0897f9b6d9aa13
Petris Fernandes <petrisrf@gmail.com>
2026-01-12 00:51:10 -0300

    Created footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "monaco-editor/esm/vs/editor/contrib/snippet/browser/snippetController2.j
 import MainPage from "./pages/MainPage";
 import Exercise from "./pages/Exercise";
 import { Route, Routes } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import rubyLogo from "./assets/ruby.png";
 
 const App = () => {
   return (
@@ -11,6 +13,23 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/exercise/:id" element={<Exercise />} />
       </Routes>
+      <div className="py-8 text-white">
+        <p className="flex items-center justify-center gap-2">
+          Made with
+          <img
+            src={reactLogo}
+            alt="React"
+            className="inline-block h-5 w-5"
+          />
+          and
+          <img
+            src={rubyLogo}
+            alt="Ruby"
+            className="inline-block h-5 w-5"
+          />
+          by Petris Rodrigo Fernandes
+        </p>
+      </div>
     </>
   );
 }
