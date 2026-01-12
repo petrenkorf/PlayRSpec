04666d87e45eb10e7110ec16628f89215b4d67a7
Petris Fernandes <petrisrf@gmail.com>
2026-01-12 00:18:14 -0300

    Added a logo

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 import lessons from "../Lessons"
+import logo from "../assets/PlayRSpec.png"
 
 const ExerciseCard = ({ id, title, description, available }) => {
   const availability = id <= 15;
-
 
   return (
     <Link to={`/exercise/${id}`} className={`text-white ${availability ? "" : "opacity-25"}`}>
@@ -18,6 +18,7 @@ const ExerciseCard = ({ id, title, description, available }) => {
 const MainPage = () => {
   return (
     <div className="bg-zinc-800">
+      <img src={logo} />
       <span className="text-white">List Exercises</span>
 
       <div className="grid grid-cols-1 max-w-[960px] m-auto">
